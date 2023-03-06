513fdf754638f9708d4af149cf77fc54e71d2349
Hyunndy <yoosa3004@naver.com>
2023-03-07 00:12:24 +0900

    백준 브론즈5 - A/B

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -9,3 +9,21 @@ import Foundation
 
 print("Hello, World!")
 
+/**
+ Swift 입력 처리 방법
+ 노말: readLine()
+ */
+
+var substring = [Int]()
+while substring.count < 2 {
+    let input = readLine()!
+    let splitArray = input.split(separator: " ").map({ Int($0) })
+    for string in splitArray {
+        if let string = string, string > 0 && string < 10 {
+            substring.append(string)
+        }
+    }
+}
+
+let float: Double = Double(substring[0]) / Double(substring[1])
+print(float)
