ad778be9dd3a0e7307c616324cf2dc0339b97ea1
Hyunndy <yoosa3004@naver.com>
2023-03-07 00:43:26 +0900

    백준 브론즈5 두 수 비교하기

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -5,25 +5,42 @@
 //  Created by hyunndy on 2023/03/06.
 //
 
-import Foundation
-
-print("Hello, World!")
-
 /**
  Swift 입력 처리 방법
  노말: readLine()
+ 지수 처리: pow
+ Int형은 -9 * 10(18승) ~ 9 * 10(18승)
  */
+
+
+import Foundation
 
 var substring = [Int]()
 while substring.count < 2 {
     let input = readLine()!
     let splitArray = input.split(separator: " ").map({ Int($0) })
     for string in splitArray {
-        if let string = string, string > 0 && string < 10 {
-            substring.append(string)
+        
+        if let string = string, string >= -100000 && Double(string) <= 100000 {
+            if substring.count > 1 {
+                if substring[0] >= string {
+                    substring.append(string)
+                }
+            } else {
+                substring.append(string)
+            }
         }
     }
 }
 
-let float: Double = Double(substring[0]) / Double(substring[1])
-print(float)
+let tuple = (substring[0], substring[1])
+switch tuple {
+case let (x,y) where x > y:
+    print(">")
+case let (x,y) where x < y:
+    print("<")
+case let (x,y) where x == y:
+    print("==")
+default:
+    break
+}
