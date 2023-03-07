b7d251a409a829d18f2b83991f02d672b826dccc
Hyunndy <yoosa3004@naver.com>
2023-03-07 11:35:53 +0900

    프로그래머스 최소 값

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -10,30 +10,16 @@
  노말: readLine()
  지수 처리: pow
  Int형은 -9 * 10(18승) ~ 9 * 10(18승)
+ 
+ 두 배열을 곱해서 최소값을 구하려면 최소 * 최대를 곱해야함
  */
 
 
 import Foundation
 
-var s = ""
-var ans:Bool = true
-var chStack = Stack<Character>()
-for c in s {
-    if c == "(" {
-        chStack.push(c)
-    } else if c == ")" {
-        // 없는데 나온 경우
-        if chStack.isEmpty == true {
-            ans = false
-            break
-        } else {
-            chStack.pop()
-        }
-    }
-}
+a = A.sorted(by: { $0 < $1 })
+b = B.sorted(by: { $0 > $1 })
 
-if chStack.count > 0 {
-    ans = false
-} else {
-    ans = true
+for (idx, item) in a.enumerated() {
+    ans += item * b[idx]
 }
