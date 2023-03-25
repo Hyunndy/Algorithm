18c9ad9a5e2f053c6ee3ca82bb2ecc7547e2b89b
Hyunndy <yoosa3004@naver.com>
2023-03-26 00:36:59 +0900

    암시적 그래프 number of Islands 너무 어렵당

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -54,59 +54,133 @@ import Foundation
 
 
 //Definition for a binary tree node.
-public class TreeNode {
-    public var val: Int
-    public var left: TreeNode?
-    public var right: TreeNode?
-    public init(_ val: Int) {
-        self.val = val
-        self.left = nil
-        self.right = nil
+//public class TreeNode {
+//    public var val: Int
+//    public var left: TreeNode?
+//    public var right: TreeNode?
+//    public init(_ val: Int) {
+//        self.val = val
+//        self.left = nil
+//        self.right = nil
+//    }
+//}
+//
+//class Solution {
+//
+//    // 깊이 - DFS 버전
+//    //    func maxDepth(_ root: TreeNode?) -> Int {
+//    //        if root == nil {
+//    //            return 0
+//    //        }
+//    //
+//    //        let left = maxDepth(root?.left)
+//    //        let right = maxDepth(root?.right)
+//    //
+//    //        return max(left, right) + 1
+//    //    }
+//
+//
+//    // 깊이 - BFS
+//    func maxDepth(_ root: TreeNode?) -> Int {
+//
+//        var depth = 0
+//
+//        if root == nil {
+//            return 0
+//        }
+//
+//        root?.val = 1
+//        var q = Queue(enQueue: [root])
+//        while !q.isEmpty {
+//            let curNode = q.pop()
+//            if curNode != nil {
+//                depth = max(depth, curNode?.val ?? 0)
+//            }
+//
+//            if curNode?.left != nil {
+//                curNode?.left?.val = depth + 1
+//                q.push(element: curNode?.left)
+//            }
+//            if curNode?.right != nil {
+//                curNode?.right?.val = depth + 1
+//                q.push(element: curNode?.right)
+//            }
+//        }
+//
+//        return depth
+//    }
+//}
+
+/*
+ Input: grid = [
+ ["1","1","1","1","0"],
+ ["1","1","0","1","0"],
+ ["1","1","0","0","0"],
+ ["0","0","0","0","0"]
+ ]
+ */
+
+
+/**
+ 1) 암시적 그래프는 내가 직접 다음 노드를 지정해줘야 한다.
+ 2) BFS, DFS는 끝날 수 있다. 왜냐? 암시적 그래프니까 이어져 있지 않기 때문에
+ 3) 암시적 그래프는 visited 도 똑같은 형태로 만들어줘야 한다.
+ 4) 암시적 그래프는 완전 탐색을 위해 m*n 이중 for문 돌아야 한다.
+ */
+class Solution {
+
+    func numIslands(_ grid: [[Character]]) -> Int {
+    
+        var numOfIsland = 0
+        let col = grid.count // 4
+        let row = grid[0].count // 5
+        var visited = Array(repeating: Array(repeating: false,count: row), count: col)
+        
+        func bfs(curLoc: (Int, Int)) {
+            
+            // 일단 방문부터?
+            visited[curLoc.0][curLoc.1] = true
+            // 상하좌우 이동
+            var dx = [-1, 1, 0, 0]
+            var dy = [0, 0, 1, -1]
+            var q = Queue(enQueue: [(curLoc.0, curLoc.1)])
+            while !q.isEmpty {
+                let (curX, curY) = q.pop()
+                
+                for idx in 0..<4 {
+                    let nextX = curX + dx[idx]
+                    let nextY = curY + dy[idx]
+                    
+                    if nextX >= 0 && nextX < col && nextY >= 0 && nextY < row {
+                        if grid[nextX][nextY] == "1" && visited[nextX][nextY] == false {
+                            visited[nextX][nextY] = true
+                            q.push(element: (nextX, nextY))
+                        }
+                    }
+                }
+            }
+        }
+        
+        for colIdx in 0..<col {
+            for rowIdx in 0..<row {
+                // 1이 아니거나 한 번 방문한 곳은 방문하면 안됨
+                if grid[colIdx][rowIdx] == "1" && visited[colIdx][rowIdx] == false {
+                    bfs(curLoc: (colIdx, rowIdx))
+                    numOfIsland += 1
+                }
+            }
+        }
+        
+        return numOfIsland
     }
 }
 
-class Solution {
-    
-    // 깊이 - DFS 버전
-    //    func maxDepth(_ root: TreeNode?) -> Int {
-    //        if root == nil {
-    //            return 0
-    //        }
-    //
-    //        let left = maxDepth(root?.left)
-    //        let right = maxDepth(root?.right)
-    //
-    //        return max(left, right) + 1
-    //    }
-    
-    
-    // 깊이 - BFS
-    func maxDepth(_ root: TreeNode?) -> Int {
-        
-        var depth = 0
-        
-        if root == nil {
-            return 0
-        }
-        
-        root?.val = 1
-        var q = Queue(enQueue: [root])
-        while !q.isEmpty {
-            let curNode = q.pop()
-            if curNode != nil {
-                depth = max(depth, curNode?.val ?? 0)
-            }
-            
-            if curNode?.left != nil {
-                curNode?.left?.val = depth + 1
-                q.push(element: curNode?.left)
-            }
-            if curNode?.right != nil {
-                curNode?.right?.val = depth + 1
-                q.push(element: curNode?.right)
-            }
-        }
-        
-        return depth
-    }
-}
+let a = Solution().numIslands([
+    ["1","1","1","1","0"],
+    ["1","1","0","1","0"],
+    ["1","1","0","0","0"],
+    ["0","0","0","0","0"]
+])
+
+print(a)
+
